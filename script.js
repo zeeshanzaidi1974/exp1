d5b93005fc6276e8a44f1195a08248f3dafe10de
zeeshanzaidi1974 <98213611+zeeshanzaidi1974@users.noreply.github.com>
2022-06-12 08:29:29 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
 
+var NetBalance=100;
+
+
+
+function multiplyBy()
+     {
+        num1 = document.getElementById("firstNumber").value;
+        num2 = document.getElementById("secondNumber").value;
+          if(num1=='0'){ 
+		alert ( 'account number is required' ) ;
+                      }
+          if(num1=='1') 
+		      {    
+		          var Deposit = prompt("Enter the first number", "");
+		       
+		alert ( 'deposit' + Deposit) ;         
+			}   
+			
+			NetBalance =              document.getElementById("").innerHTML = NetBalance + Deposit;
+			
+			alert ( 'Net balance' + NetBalance) ;
+           if(num1=='2') 
+		      {  
+		      
+		        
+		  alert ( 'account number is required' ) ;}
+                 
+                      
+                                                        				
+              document.getElementById("result").innerHTML = num1 * num2;
+}
+
+function divideBy() 
+{ 
+        num1 = document.getElementById("firstNumber").value;
+        num2 = document.getElementById("secondNumber").value;
+document.getElementById("result").innerHTML = num1 / num2;
+}
+
